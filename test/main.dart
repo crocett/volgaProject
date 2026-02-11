0daf2c32e60ab20c8465e834995d4c9f50a1a059
abcdefg_minSUng <vikavolk531@mail.ru>
2026-02-11 11:44:06 +0300

    просто добавление главной страницы без чего-либо

---
+++ test/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'hello_window.dart';
+//import 'package:volga/main.dart';
+
+void main() async {
+  runApp(const MaterialApp(home: HomeWindowScreen()));
+}
